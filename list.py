78efefbe12305a85fbd06eca9ed80a296ef936e2
adira29 <iamaditirani29@gmail.com>
2025-11-11 19:50:46 +0530

    add all changes so far

--- list.py
+++ list.py
@@ -1,3 +1,24 @@
 friends = ["Apple","Orange",5,345.06,"Ram","Rohan"]
 print(friends[0])
 
+print(friends[3])
+friends[4]="y"
+print(friends[4]) #unlike strings, lists are mutable
+
+print(friends[1:4])
+
+friends.append("Ram")
+print(friends)
+l1=[1,8,9,56,6,3]
+l1.sort()
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.insert(3,8)
+print(l1)
+
+l1.pop()
+print(l1)
+
