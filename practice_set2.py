71d58eebfd71cc91ce664d97f42d1fcebe22a9d8
adira29 <iamaditirani29@gmail.com>
2025-11-11 11:30:13 +0530

    updating new program

---
+++ practice_set2.py
@@ -0,0 +1,36 @@
+# 1 Problem 1
+
+a= 1
+b=5
+print(a+b)
+
+# 2 problem
+
+a=34
+b=5
+print("Reminder when a is divided by b is", a%b)
+
+# 3 problem
+
+a = input("enter the value of a : ")
+print(type(a))
+
+# 4 problem
+
+a=int(input("enter no1:"))
+b=int(input("enter b : "))
+
+print("a is greater than b is ",a>b)
+
+
+# 5 problem
+
+a= int(input("enter no. 1: "))
+b = int(input("enter no.2 : "))
+print("the average of these two nos. is ", (a+b)/2)
+
+# 6 problem
+
+
+a= int(input("enter no. 1: "))
+print("te square of the numbers are ", a**2)
