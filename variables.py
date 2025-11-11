c4be10f01c214a9470641183ded9cc3206af99a9
adira29 <iamaditirani29@gmail.com>
2025-11-11 10:01:01 +0530

    Updating with new changes

--- variables.py
+++ variables.py
@@ -1,3 +1,36 @@
 a = 1
 b = 2
+c = 7
+name = "harry"
 print(a+b)
+print(name)
+d = False
+e= None
+#  Arthmetic operators
+# assignment operator
+a = 4-2
+b = 6
+b+=3 
+print(b) 
+
+#  comparison operators
+
+d = 5 >4
+print(d)
+
+# logical operators
+
+e = True or False
+print(e)
+
+print(not False)
+
+a = 31
+print(type(a))
+
+a = int(input("enter number 1: "))
+b = int(input("enter number 2:  "))
+
+print("number 1 ", a)
+print("number 2", b)
+print("sum is ",a+b)
