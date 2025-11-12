2937d5ba45ca6912bb49c9ba9c2d20e8bf7f3c36
adira29 <iamaditirani29@gmail.com>
2025-11-12 21:12:22 +0530

    adding some sets docs

--- dictionary_sets.py
+++ dictionary_sets.py
@@ -22,3 +22,41 @@ print(marks.values())
 marks.update({"Ram":99})
 print(marks)
  
+print(marks.get("Ram2")) #prints None
+print(marks["Ram2"]) #returns an error 
+
+d={} # Empty dictionary
+print(type(d))
+
+
+# sets - contains unique value , didn't maintain any order , if you want order use list
+
+
+
+s = {1,5,32}
+e = set() #empty set
+
+print(type(e))
+# Don't use s ={} as it will create an empty dictionary
+
+
+# sets methods
+
+s = {1,5,32,54,"Ram",5,5,6}
+s.add(566)
+print(s,type(s))
+
+s.remove(1)
+print(s)
+
+s.pop()
+print(s)
+
+s1 = {1,45,6}
+s2={7,8,1,780}
+
+print(s1.union(s2))
+
+print(s1.intersection(s2))
+
+
