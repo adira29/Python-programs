934130c05ddf4588c8f6e8ad221edafd1a914d50
adira29 <iamaditirani29@gmail.com>
2025-11-14 13:25:27 +0530

    adding new project

--- project1/main.py
+++ project1/main.py
@@ -20,21 +20,21 @@ if(computer == you):
 else:
 
 
-  if(computer ==-1 and you ==1):
+  if(computer == -1 and you == 1):
     print("You win")
 
-  elif(computer ==-1 and you ==0):
+  elif(computer == -1 and you == 0):
     print("You lose!")
 
-  elif(computer ==1 and you ==-1):
+  elif(computer == 1 and you == -1):
     print("You Lose!")
 
-  elif(computer ==1 and you ==0):
+  elif(computer == 1 and you == 0):
     print("You Win")
 
-  elif(computer ==0 and you ==-1):
+  elif(computer == 0 and you == -1):
     print("You win!")
-  elif(computer ==0 and you ==1):
+  elif(computer == 0 and you == 1):
     print("You lose!")
 
   else:
