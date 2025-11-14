ae7567f2c5ccf5960c40136bc040b09ef1a3a7e7
adira29 <iamaditirani29@gmail.com>
2025-11-14 11:13:14 +0530

    adding new files

---
+++ functions.py
@@ -0,0 +1,31 @@
+# a = int(input("enter the number : "))
+# b = int(input("enter the number : "))
+# c = int(input("enter the number : "))
+
+# average = (a+b+c)/3
+
+# print(average)
+
+
+# function defination
+def avg():
+    a = int(input("enter the number : "))
+    b = int(input("enter the number : "))
+    c = int(input("enter the number : "))
+
+    average = (a+b+c)/3
+
+    print(average)
+
+avg() #function call
+
+
+def greet(name):
+    print("good day, " + name)
+
+greet("Adira")
+
+
+
+
+
