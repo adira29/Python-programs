6c683c2f17db7a21cf81cde8bd163d0729ca8af4
adira29 <iamaditirani29@gmail.com>
2025-11-14 11:49:28 +0530

    modified functions

--- functions.py
+++ functions.py
@@ -20,12 +20,30 @@ def avg():
 avg() #function call
 
 
-def greet(name):
+def greet(name, ending):
     print("good day, " + name)
+    print(ending)
+    return "done"
 
-greet("Adira")
+a = greet("Adira","Thanks")
+print(a)
 
+def goodDay(name, ending="Thank you"):
+    print(f"Good Day, {name}")
+    print(ending)
 
+goodDay("Harry")
+goodDay("Rohan", "thanks")
+
+# recursion
+
+def factorial(n):
+    if(n==1 or n==0):
+        return 1
+    return n* factorial(n-1)
+
+n = int(input("enter a number : "))
+print(f"the factorial of this number is : {factorial(n)}")
 
 
 
