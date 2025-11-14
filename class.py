00a3eb39046ac1ffcb8ccf1e178125e3a80be167
adira29 <iamaditirani29@gmail.com>
2025-11-14 20:39:48 +0530

    new class file

--- class.py
+++ class.py
@@ -3,6 +3,11 @@ class Employee:
     salary = 1200000
 
     
+
+    def __init__(self): #dunder method which is automatically called
+       print("I am creating an object")
+
+
     def getInfo(self):
         print(f"The language is {self.language} . The salary is {self.salary}")
     
@@ -19,7 +24,4 @@ print(harry.language,harry.name,harry.salary)
 rohan = Employee()
 rohan.name = "Rohan"
 print(rohan.salary,rohan.name, rohan.language)
-
-
-
 harry.getInfo()
